df3d03ef1afe718f7811faf2427ed6c11aad0a9e
= <=>
2021-06-04 11:29:38 +0700

    film pembenahan

---
+++ lib/model/film.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
+
+class Film{
+  String nm, ia, ln, tp, cr, dt, tm, sr, sa, bo, bl, lc, qc;
+  Film({this.nm, this.ia, this.ln, this.tp, this.cr, this.dt, this.tm, this.sr, this.sa, this.bo, this.bl, this.lc, this.qc});
+  factory Film.fromJson(Map<String, dynamic> p){
+    return Film(nm:p['nm'],ia:p['ia'],ln:p['ln'],tp:p['tp'],cr:p['cr'],dt:p['dt'],tm:p['tm'],sr:p['sr'],sa:p['sa'],bo:p['bo'],bl:p['bl'],lc:p['lc'],qc:p['qc']);
+  }
+}
+Future<String> loadAsset() async{return await rootBundle.loadString('assets/data/flim.json');}
+Future<Film> loadFlim() async{return new Film.fromJson(json.decode(await loadAsset()));}
